3ece3192a9cb89601db811b1a0c3a4ab9c41223e
Javier García Latorre <garcialatorre365@gmail.com>
2022-09-26 22:07:58 +0200

    feat(header): render list of links

--- components/navbar/header.tsx
+++ components/navbar/header.tsx
@@ -1,16 +1,28 @@
-
+import React from "react"
+import { navLinks } from "./data"
+import Link from "next/link"
 
 const Header = () => {
-    return(
-    <header>
-        <section>
-            <h3>Javier Garcia</h3>
-        </section>
-        <span className='fill-space' />
-        <section>
-            <nav></nav>
-        </section>
-    </header>)
+    return (
+        <header>
+            <section>
+                <h3>Javier Garcia</h3>
+            </section>
+            <span className='fill-space' />
+            <section>
+                <nav>
+                    <ul>
+                        {navLinks.map((link, index) => {
+                            return (
+                                <Link href={link.path} key={index}>
+                                    <li >{link.name}</li>
+                                </Link>
+                            )
+                        })}
+                    </ul>
+                </nav>
+            </section>
+        </header>)
 }
 
 export default Header
